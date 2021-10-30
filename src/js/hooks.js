8ef503da94c040b91d2373ce46c4d42f92af602f
ch3cknull <ch3cknull@outlook.com>
2021-10-30 16:48:25 +0800

    ✨ feat: add copy hook

--- src/js/hooks.js
+++ src/js/hooks.js
@@ -1,9 +1,13 @@
+import clipboard from 'clipboard'
+
+// 通过UA判断是否是移动端
 function isMobileUserAgent() {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
   )
 }
 
+// 判断是否是移动端
 function isMobilePlatform() {
   const plt = navigator.platform
   return (
@@ -14,3 +18,27 @@ function isMobilePlatform() {
 }
 
 export const useMobile = isMobilePlatform() && isMobileUserAgent()
+
+/**
+ * 复制指定DOM元素的内容
+ * @param {*} selector        复制按钮选择器
+ * @param {*} targetSelector  复制目标选择器
+ * @param {*} success         成功回调
+ * @param {*} error           错误回调
+ * @returns
+ */
+export function useCopy(selector, targetSelector, success, error) {
+  return () => {
+    const target = document.querySelector(targetSelector)
+    const cp = new clipboard(selector || '#copy', {
+      text: () => target.innerText,
+    })
+    cp.on('success', () => {
+      success && success()
+      cp.destroy()
+    })
+    cp.on('error', () => {
+      error && error()
+    })
+  }
+}
