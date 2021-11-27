8d9aab2eebba42cdcfd798df28a753b489ddfdb5
ch3cknull <ch3cknull@outlook.com>
2021-11-27 19:47:59 +0800

    feat: update api

--- src/js/main.js
+++ src/js/main.js
@@ -24,17 +24,37 @@ window.copy = useCopy(
   }
 )
 
-window.onButtonClick = debounce(async () => {
+const verify = debounce(async () => {
   const tokenCode = sessionStorage.getItem('token')
-  const { isAuthed } = await verifyToken(tokenCode)
-  if (isAuthed) {
+  const { data } = await verifyToken(tokenCode)
+  console.log(data)
+  if (data && data.code == 0) {
     Cookies.set('token', tokenCode, { expires: 1 })
+    Cookies.set('time', new Date() * 1, { expires: 1 })
     toast('验证成功', 'success')
-    redirect()
+    setTimeout(() => {
+      location.reload()
+    }, 2500)
   } else {
     toast('验证失败，token已刷新，请重试', 'error')
     await flushToken()
   }
+}, 600)
+
+const confirm = debounce(async () => {
+  const redirect = function () {
+    const token = Cookies.get('token')
+    const urlParams = getUrlParam()
+    if (!urlParams.redirect_uri) {
+      return
+    }
+
+    window.location.href = urlParams.redirect_uri + '#token=' + token
+    throw 'stop exec' //中止代码继续执行
+  }
+
+  toast('将为您自动跳转', 'success')
+  setTimeout(redirect, 2500)
 }, 600)
 
 // 用于自动打开私聊窗口
@@ -62,38 +82,44 @@ async function flushToken() {
   sessionStorage.setItem('token', tokenCode)
 }
 
-window.redirect = function () {
-  const token = Cookies.get('token')
-  const urlParams = getUrlParam()
-  if (!urlParams.redirect_uri) {
-    return
-  }
-
-  window.location.href = urlParams.redirect_uri + '#token=' + token
-  throw 'stop exec' //中止代码继续执行
-}
 window.isTokenValid = verifyToken
 
 async function init() {
   const token = Cookies.get('token')
-  const { isAuthed, nickname, uid, createTs } = await verifyToken(token)
-  if (token && isAuthed) {
+  if (!token) {
+    await flushToken()
+    element.submitButton.onclick = verify
+    return
+  }
+  const { data } = await verifyToken(token)
+  if (data && data.code == 0) {
+    const time = parseInt(Cookies.get('time')) + 86400000
     element.board.style.display = 'none'
     element.submitButton.innerText = '允许获取'
     element.submitButton.className += ' mt-4'
-    element.bottomText.innerText = `${nickname}(${uid})\n您已授权，授权后 24小时 内您无需再次认证\n授权时间:${parseTime(
-      createTs
-    )}`
+    element.submitButton.onclick = confirm
+    element.bottomText.innerText = `用户 UID${
+      data.uid
+    }\n您已授权，授权后 24小时 内您无需再次认证,\n 授权过期时间 ${parseTime(
+      time
+    )}\n您也可以点击下方 红色按钮 手动取消授权`
     const revokeButton = document.createElement('button')
-    revokeButton.onclick = () => {
-      revokeToken(token, uid)
+    revokeButton.onclick = async () => {
+      const { data } = await revokeToken(token)
+      if (data.code != 0) {
+        toast('当前未登录', 'error')
+        return
+      }
       Cookies.remove('token')
+      Cookies.remove('time')
+      toast('注销成功', 'success')
+      setTimeout(() => {
+        location.reload()
+      }, 2500)
     }
     revokeButton.innerText = '取消授权'
-    revokeButton.className = 'btn bg-red-400'
+    revokeButton.className = 'btn bg-red-400 hover:bg-red-600'
     document.querySelector('#main-content').appendChild(revokeButton)
-  } else {
-    await flushToken()
   }
 }
 
